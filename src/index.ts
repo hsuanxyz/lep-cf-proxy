47e2c620a2fe7e7e8a5441ec43fce8ba696ce259
Hsuan Lee <hsuangm@gmail.com>
2024-01-25 15:23:52 +0800

    feat: add api proxy

--- src/index.ts
+++ src/index.ts
@@ -23,10 +23,49 @@ export interface Env {
 	//
 	// Example binding to a Queue. Learn more at https://developers.cloudflare.com/queues/javascript-apis/
 	// MY_QUEUE: Queue;
+	API_URL: string;
+	API_TOKEN: string;
 }
 
+const CORS_HEADERS: Record<string, string> = {
+	"access-control-allow-origin": "*",
+	"access-control-allow-methods": "POST, OPTIONS",
+	"access-control-allow-headers": "Content-Type, Authorization",
+};
+
 export default {
-	async fetch(request: Request, env: Env, ctx: ExecutionContext): Promise<Response> {
-		return new Response('Hello World!');
+	async fetch(req: Request, env: Env, ctx: ExecutionContext): Promise<Response> {
+		if (req.method === "OPTIONS") {
+			return new Response(null, {
+				headers: CORS_HEADERS,
+			});
+		}
+		const { pathname, search } = new URL(req.url);
+		const url = new URL(pathname + search, env.API_URL).href;
+
+		const headers = new Headers();
+		headers.set("Accept", `image/jpeg`);
+		headers.set("Authorization", `Bearer ${env.API_TOKEN}`);
+		headers.set(
+			"Content-Type",
+			req.headers.get("Content-Type") || "application/json",
+		);
+
+		const res = await fetch(url.toString(), {
+			body: req.body,
+			method: req.method,
+			headers,
+		});
+
+		const resHeaders = new Headers(res.headers);
+		Object.keys(CORS_HEADERS).forEach((key) => {
+			resHeaders.set(key, CORS_HEADERS[key]);
+		});
+
+		return new Response(res.body, {
+			status: res.status,
+			headers: resHeaders,
+		})
+
 	},
 };
